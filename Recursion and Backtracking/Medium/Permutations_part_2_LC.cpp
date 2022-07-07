9cf27b6aea92f0eacfd5cbe56b8b8ee1cf49bc4b
Prakhar Dhama <pdhama161@gmail.com>
2022-07-07 13:16:04 +0530

    just a check

---
+++ Recursion and Backtracking/Medium/Permutations_part_2_LC.cpp
@@ -0,0 +1,19 @@
+/*
+    Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+    Input: nums = [1,2,3]
+    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]   
+
+    Input: nums = [0,1]
+    Output: [[0,1],[1,0]]
+
+    ** This solution uses less space than the previous part-1.
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    
+};
